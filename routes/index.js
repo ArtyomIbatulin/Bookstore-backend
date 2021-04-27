f720f6100e849bb547f004b6ceb7f0b9f6669c80
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-04-27 14:34:05 +0300

    fixed controllers import

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,37 @@ const router = require('express').Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
 const categoryController = require('../controllers/categoryController');
-const filmController = require('../controllers/filmController');
+const authorController = require('../controllers/authorController');
+const bookController = require('../controllers/bookController');
+const commentController = require('../controllers/commentController');
+const ratingController = require('../controllers/ratingController');
 const healthController = require('../controllers/healthController');
 
 router.get('/api/v1/health', healthController.health);
 
-router.post('/api/v1/createAuthor', filmController.createAuthor);
-router.get('/api/v1/findAuthor', filmController.findAuthors);
-router.get('/api/v1/findAuthor/:id', filmController.findAuthor);
-router.delete('/api/v1/findAuthor/:id', filmController.deleteAuthor);
-router.put('/api/v1/putAuthor/:id', filmController.putAuthor);
+router.post('/api/v1/createAuthor', authorController.createAuthor);
+router.get('/api/v1/findAuthor', authorController.findAuthors);
+router.get('/api/v1/findAuthor/:id', authorController.findAuthor);
+router.delete('/api/v1/findAuthor/:id', authorController.deleteAuthor);
+router.put('/api/v1/putAuthor/:id', authorController.putAuthor);
 
-router.post('/api/v1/createBook', filmController.createBook);
-router.get('/api/v1/findBook', filmController.findBooks);
-router.get('/api/v1/findBook/:id', filmController.findBook);
-router.delete('/api/v1/findBook/:id', filmController.deleteBook);
-router.put('/api/v1/putBook/:id', filmController.putBook);
+router.post('/api/v1/createBook', bookController.createBook);
+router.get('/api/v1/findBook', bookController.findBooks);
+router.get('/api/v1/findBook/:id', bookController.findBook);
+router.delete('/api/v1/findBook/:id', bookController.deleteBook);
+router.put('/api/v1/putBook/:id', bookController.putBook);
 
-router.post('/api/v1/createComment', filmController.createComment);
-router.get('/api/v1/findComment', filmController.findComments);
-router.get('/api/v1/findComment/:id', filmController.findComment);
-router.delete('/api/v1/findComment/:id', filmController.deleteComment);
-router.put('/api/v1/putComment/:id', filmController.putComment);
+router.post('/api/v1/createComment', commentController.createComment);
+router.get('/api/v1/findComment', commentController.findComments);
+router.get('/api/v1/findComment/:id', commentController.findComment);
+router.delete('/api/v1/findComment/:id', commentController.deleteComment);
+router.put('/api/v1/putComment/:id', commentController.putComment);
 
-router.post('/api/v1/createRating', filmController.createRating);
-router.get('/api/v1/findRating', filmController.findRatings);
-router.get('/api/v1/findRating/:id', filmController.findRating);
-router.delete('/api/v1/findRating/:id', filmController.deleteRating);
-router.put('/api/v1/putRating/:id', filmController.putRating);
+router.post('/api/v1/createRating', ratingController.createRating);
+router.get('/api/v1/findRating', ratingController.findRatings);
+router.get('/api/v1/findRating/:id', ratingController.findRating);
+router.delete('/api/v1/findRating/:id', ratingController.deleteRating);
+router.put('/api/v1/putRating/:id', ratingController.putRating);
 
 router.post('/api/v1/sign-up', userController.registration);
 router.post('/api/v1/sign-in', userController.login);
