50e9bc842c71c03a0b9de5d640ce81a201a9e997
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-04-28 12:33:05 +0300

    fixed routes paths

--- routes/index.js
+++ routes/index.js
@@ -10,41 +10,44 @@ const healthController = require('../controllers/healthController');
 
 router.get('/api/v1/health', healthController.health);
 
-router.post('/api/v1/createAuthor', authorController.createAuthor);
-router.get('/api/v1/findAuthor', authorController.findAuthors);
-router.get('/api/v1/findAuthor/:id', authorController.findAuthor);
-router.delete('/api/v1/findAuthor/:id', authorController.deleteAuthor);
-router.put('/api/v1/putAuthor/:id', authorController.putAuthor);
+router.post('/api/v1/create-author', authorController.createAuthor);
+router.get('/api/v1/get-author', authorController.findAuthors);
+router.get('/api/v1/find-author/:id', authorController.findAuthor);
+router.delete('/api/v1/find-author/:id', authorController.deleteAuthor);
+router.put('/api/v1/change-author/:id', authorController.putAuthor);
 
-router.post('/api/v1/createBook', bookController.createBook);
-router.get('/api/v1/findBook', bookController.findBooks);
-router.get('/api/v1/findBook/:id', bookController.findBook);
-router.delete('/api/v1/findBook/:id', bookController.deleteBook);
-router.put('/api/v1/putBook/:id', bookController.putBook);
+router.post('/api/v1/create-book', bookController.createBook);
+router.get('/api/v1/get-book', bookController.findBooks);
+router.get('/api/v1/find-book/:id', bookController.findBook);
+router.delete('/api/v1/find-book/:id', bookController.deleteBook);
+router.put('/api/v1/change-book/:id', bookController.putBook);
 
-router.post('/api/v1/createComment', commentController.createComment);
-router.get('/api/v1/findComment', commentController.findComments);
-router.get('/api/v1/findComment/:id', commentController.findComment);
-router.delete('/api/v1/findComment/:id', commentController.deleteComment);
-router.put('/api/v1/putComment/:id', commentController.putComment);
+router.post('/api/v1/create-comment', commentController.createComment);
+router.get('/api/v1/get-comment', commentController.findComments);
+router.get('/api/v1/find-comment/:id', commentController.findComment);
+router.delete('/api/v1/find-comment/:id', commentController.deleteComment);
+router.put('/api/v1/change-comment/:id', commentController.putComment);
 
 // user-rating
 // показывает 1 категорию на книге ...
-router.post('/api/v1/createRating', ratingController.createRating);
-router.get('/api/v1/findRating', ratingController.findRatings);
-router.get('/api/v1/findRating/:id', ratingController.findRating);
-router.delete('/api/v1/findRating/:id', ratingController.deleteRating);
-router.put('/api/v1/putRating/:id', ratingController.putRating);
+// rating orders
+// seq pagination
+// videos
+router.post('/api/v1/create-rating', ratingController.createRating);
+router.get('/api/v1/get-rating', ratingController.findRatings);
+router.get('/api/v1/find-rating/:id', ratingController.findRating);
+router.delete('/api/v1/find-rating/:id', ratingController.deleteRating);
+router.put('/api/v1/change-rating/:id', ratingController.putRating);
 
 router.post('/api/v1/sign-up', userController.registration);
 router.post('/api/v1/sign-in', userController.login);
 router.get('/api/v1/auth', authMiddleware, userController.check);
-router.get('/api/v1/getUsers', userController.getUsers);
+router.get('/api/v1/get-users', userController.getUsers);
 
-router.post('/api/v1/createCategory', categoryController.createCategory);
-router.put('/api/v1/changeCategory/:id', categoryController.changeCategory);
-router.delete('/api/v1/delCategory/:id', categoryController.deleteCategory);
-router.get('/api/v1/findCategory', categoryController.findCategory);
-router.get('/api/v1/findCategory/:id', categoryController.findOneCategory);
+router.post('/api/v1/create-category', categoryController.createCategory);
+router.put('/api/v1/change-category/:id', categoryController.changeCategory);
+router.delete('/api/v1/del-category/:id', categoryController.deleteCategory);
+router.get('/api/v1/get-category', categoryController.findCategory);
+router.get('/api/v1/find-category/:id', categoryController.findOneCategory);
 
 module.exports = router;
