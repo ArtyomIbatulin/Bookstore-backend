5907eb9437e55c1fdfef418a3319678fcc97d997
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-05-13 15:19:32 +0300

    included cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./db');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
