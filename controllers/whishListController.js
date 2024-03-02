1be13689b64b656eb62e2fc78a1232e342aa8f78
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-02 16:21:51 +0300

    added unlikeBook in the wishlistController

--- controllers/whishListController.js
+++ controllers/whishListController.js
@@ -31,7 +31,33 @@ const likeBook = async (req, res) => {
   }
 };
 
-const unlikeBook = async (req, res) => {};
+const unlikeBook = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  if (!id) {
+    return res.status(400).json({ message: "Уже есть дизлайк" });
+  }
+
+  try {
+    const existingLike = await db.Wihslist.findOne({
+      where: { bookId: id, userId },
+    });
+
+    if (!existingLike) {
+      return res.status(400).json({ message: "Лайка нет" });
+    }
+
+    const like = await db.Wihslist.delete({
+      where: { bookId: id, userId },
+    });
+
+    return res.json(like);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
 
 module.exports = {
   likeBook,
