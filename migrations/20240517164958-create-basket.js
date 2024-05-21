51f5dd9a0a32f11db8bc9ff04a0053deec0d398b
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-05-21 23:35:07 +0300

    fixed basket migration

--- migrations/20240517164958-create-basket.js
+++ migrations/20240517164958-create-basket.js
@@ -8,18 +8,18 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      // amount: {
-      //   type: Sequelize.INTEGER,
-      //   allowNull: false,
-      // },
-      // UserId: {
-      //   type: Sequelize.INTEGER,
-      //   references: {
-      //     model: 'Users',
-      //     key: 'id',
-      //   },
-      //   allowNull: false,
-      // },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
