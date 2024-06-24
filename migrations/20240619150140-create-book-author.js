ae10451c80d29a0696e3521f177a22f6fce112fc
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-06-24 20:28:17 +0300

    fixed relations in book-author model & migration

--- migrations/20240619150140-create-book-author.js
+++ migrations/20240619150140-create-book-author.js
@@ -8,21 +8,23 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      BookId: {
+      bookId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
         references: {
           model: "Books",
           key: "id",
         },
-        allowNull: false,
       },
-      AuthorId: {
+      authorId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
         references: {
           model: "Authors",
           key: "id",
         },
-        allowNull: false,
       },
       createdAt: {
         allowNull: false,
--- models/book_author.js
+++ models/book_author.js
@@ -2,8 +2,15 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Book_author extends Model {
-    static associate(models) {
-      // define association here
+    static associate({ Book, Author }) {
+      // Определяем ассоциацию многие-ко-многим
+      this.belongsTo(Book, {
+        foreignKey: "bookId",
+      });
+
+      this.belongsTo(Author, {
+        foreignKey: "authorId",
+      });
     }
   }
   Book_author.init(
